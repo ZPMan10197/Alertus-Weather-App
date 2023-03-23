eaab8f457bf750c002d02c0315e50563cd7742dc
Zeshawn Manzoor <zeshawnmanzoor@yahoo.com>
2023-03-23 02:33:48 -0400

    Updated Request Form

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
 from flask import Flask, render_template, request, redirect, url_for
 import requests
 
-app = Flask(__name__)
+app = Flask(__name__) # Initialize Flask app
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        # Handle form submission here!
-        pass
+        state = request.form['state']
+        urgency = request.form['urgency']
+        severity = request.form['severity']
+        certainty = request.form['certainty']
+        max_results = int(request.form['max_results'])
+
+        # Make the NOAA API request and process the results
+        # ...
+
+        return render_template('results.html', alerts=alerts)
 
     return render_template('index.html')
 
